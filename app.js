410288a5963e698b00092c96f951d47297f73b6b
Tony <extracat@users.noreply.github.com>
2023-03-08 11:47:28 +0900

    save

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+var livereload = require("livereload");
+var connectLiveReload = require("connect-livereload");
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`<html>Hello World<html>`);
 });
 
 server.listen(port, hostname, () => {
